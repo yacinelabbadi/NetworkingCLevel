adde7161d7fd62bc0d7f283c7d1dced2df550766
yacinelabbadi <yacin.elabbadi@elev.ga.ntig.se>
2024-05-20 16:08:47 +0200

    Klar med koden?

--- MulticastChatServer_Yassin/src/MulticastChatServer.java
+++ MulticastChatServer_Yassin/src/MulticastChatServer.java
@@ -2,22 +2,33 @@ import java.io.IOException;
 import java.net.*;
 
 public class MulticastChatServer {
-    public static void main(String[] args) {
-
-        try {
-            // Default port number we are going to use
-            int portNumber = 5000;
-            if (args.length >= 1) {
-                portNumber = Integer.parseInt(args[0]);
-            }
-
-            // Create a MulticastSocket
-            MulticastSocket serverMulticastSocket = new MulticastSocket(portNumber);
-            System.out.println("MulticastSocket is created at port " + portNumber);
-
-            // Determine he IP address of a host, given the host time
-        } catch(IOException ie) {
-
+    public static void main(String[] args) throws Exception {
+        // Default port number we are going to use
+        int portNumber = 5000;
+        if (args.length >= 1) {
+            portNumber = Integer.parseInt(args[0]);
         }
+
+        // Create a MulticastSocket
+        MulticastSocket serverMulticastSocket = new MulticastSocket(portNumber);
+        System.out.println("MulticastSocket is created at port " + portNumber);
+
+        // Determine he IP address of a host, given the host name
+        InetAddress group = InetAddress.getByName("225.4.5.6");
+
+        // getByName- returns IP address of given host
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+        boolean infinite = true;
+
+        // Continually recieves data and prints them
+        while(infinite) {
+            byte buf[] = new byte[1024];
+            DatagramPacket data = new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg = new String(data.getData()).trim();
+            System.out.println("Message recieved from client = " + msg);
+        }
+        serverMulticastSocket.close();
     }
 }
